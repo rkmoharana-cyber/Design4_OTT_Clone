e2678e950d11ea86eef5502302d98dbbedf99998
Rati Krishna Moharana <73690698+rkmoharana-cyber@users.noreply.github.com>
2024-04-12 12:16:23 +0530

    Required Files Added

---
+++ assets/js/api.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const api_key = 'a0e43390fc3331d4dfa3e842135b9f62';
+const imageBaseURL = 'https://image.tmdb.org/t/p/';
+
+/**
+ * fetch data from a server using the `url` and passes
+ * the result in JSON data to a `callback` function,
+ * along with an optional parameter if has `optionalParam`.
+ */
+
+const fetchDataFromServer = function (url, callback, optionalParam) {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => callback(data, optionalParam));
+}
+
+export { imageBaseURL, api_key, fetchDataFromServer };
